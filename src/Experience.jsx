f519c04a77eba40485df017fb802e89221875db5
Sirbu Gabriel <sirbugabriel97@gmail.com>
2023-07-31 19:17:59 +0300

    first commit

---
+++ src/Experience.jsx
@@ -0,0 +1,15 @@
+import { OrbitControls } from '@react-three/drei'
+
+export default function Experience()
+{
+  return <>
+      <color args= { ['#241a1a']} attach = "background"/>
+        <OrbitControls makeDefault />
+        
+        <mesh>
+            <boxGeometry />
+            <meshNormalMaterial />
+        </mesh>
+
+    </>
+}
